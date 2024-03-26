21395da1c308d487e4f49692cb0c8f05116894fa
Fardin7864 <fardinhasan1888@gmail.com>
2024-03-26 12:07:32 +0600

    make the banner and carousel responsive

--- src/components/Categories/CategorieTab.jsx
+++ src/components/Categories/CategorieTab.jsx
@@ -2,46 +2,67 @@ import { IoPizzaOutline } from "react-icons/io5";
 import { PiHamburgerThin } from "react-icons/pi";
 import { LuSalad } from "react-icons/lu";
 import { IoFastFoodOutline } from "react-icons/io5";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
-import pizza1 from "../../assets/pizza1.png";
-import pizza2 from "../../assets/pizza2.png";
-import pizza3 from "../../assets/pizza3.png";
-import pizza4 from "../../assets/pizza4.png";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 import Carousel from "./Carousel";
+import { useState } from "react";
 
 const CategorieTab = () => {
+  const [activBtn, setActiveBtn] = useState({
+    pizza: true,
+    burger: false,
+    salad: false,
+    combos: false,
+  })
+
+  const handleChange = (btn) => { 
+    if(btn === 'burger') setActiveBtn({ 
+      pizza: false,
+      burger: true,
+      salad: false,
+      combos: false,})
+    if(btn === 'salad') setActiveBtn({ 
+      pizza: false,
+      burger: false,
+      salad: true,
+      combos: false,})
+    if(btn === 'combo') setActiveBtn({ 
+      pizza: false,
+      burger: false,
+      salad: false,
+      combos: true,})
+    if(btn === 'pizza') setActiveBtn({ 
+      pizza: true,
+      burger: false,
+      salad: false,
+      combos: false,})
+   }
+
+  //  console.log(activBtn)
 
   return (
     <div className="tab-container">
       <div className="btn-container">
-        <button className="tab">
+        <button onClick={() => handleChange('pizza')} className={`tab ${activBtn.pizza ? 'active' : ''}`}>
           <IoPizzaOutline style={{ fontSize: "28px" }} />
           Pizza
         </button>
-        <button className="tab">
+        <button onClick={() => handleChange('burger')} className={`tab ${activBtn.burger ? 'active' : ''}`}>
           <PiHamburgerThin style={{ fontSize: "28px" }} />
           Burgers
         </button>
-        <button className="tab">
+        <button onClick={() => handleChange('salad')} className={`tab ${activBtn.salad ? 'active' : ''}`}>
           <LuSalad style={{ fontSize: "28px" }} />
           Salads
         </button>
-        <button className="tab">
+        <button onClick={() => handleChange('combo')} className={`tab ${activBtn.combos ? 'active' : ''}`}>
           <IoFastFoodOutline style={{ fontSize: "28px" }} />
           Combos
         </button>
       </div>
       {/* carousel */}
-      <div style={{ marginTop:'60px', width:'100%', padding:'30px 0'}}> 
+      <div style={{ maxWidth: '80vw'}}>
+      <div className="carousel-container"> 
         <Carousel/>
+      </div>
       </div>
     </div>
   );
